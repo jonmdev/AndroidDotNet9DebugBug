d82c44ccda5d0330434f8963ab7faba2c1531aa8
jonmdev <43948450+jonmdev@users.noreply.github.com>
2024-12-17 23:48:55 -0500

    Initial commit

---
+++ WebViewDebugBug/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace WebViewDebugBug
+{
+    public partial class App : Application
+    {
+        public App() {
+
+            IDispatcherTimer timer = Application.Current.Dispatcher.CreateTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += delegate {
+                Debug.WriteLine("SECOND PASSED: " + DateTime.UtcNow.Second);
+            };
+            Debug.WriteLine("START TIMER");
+            timer.Start();
+
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ WebViewDebugBug/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WebViewDebugBug
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
